6ae79b6f4c35be8500f8064284a7fc99f5431412
Dominic Tuso <dtuso@godaddy.com>
2014-05-20 11:59:51 -0700

    My first view usign EmbeddedJs

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This is the site we build in the node & express class
 
 * Node.js
 * Express.js
+* EmbeddedJs (templating system similar to ASP.NET syntax)
 * Heroku
 * Heroku Toolbelt
 
--- app.js
+++ app.js
@@ -1,34 +1,13 @@
-// var http = require('http'); //add the http module
-// var myServer = http.createServer(function(request, response) {
-//   response.writeHead(200, {"Content-Type" : "text/html"});
-//   response.write("<b>Hello</b> World 2");
-//   response.end();
-// }); //create a server
-
-
-
-// myServer.listen(3000);
-
-// console.log("Go to http://localhost:3000 on your browser");
-
-
-// var express = require('express');
-// var app = express();
-
-// app.get('/', function(req, res) {
-//   res.send('<H1>Hello</H1> Express');
-// });
-
-// var server = app.listen(3000, function() {
-//   console.log('Listening on port 3000');
-// });
-
-
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send('<H1>Hello</H1> Express');
+  res.render('default', {
+    title: 'Home',
+    users: ['Ray', 'Morten', 'James']
+  });
 });
 
 app.get('/me', function(req, res) {
@@ -46,15 +25,71 @@ app.get('/who/:name?/:title?', function(req, res) {
   res.send('<p>name: ' + name + '<br>title: ' + title + '</p>');
 });
 
-
-app.get(['/redirect','/godaddy'], function(req, res) {
-  res.redirect('http://www.godaddy.com');
-});
-
 app.get('*', function(req, res) {
-  res.send(404,'Bad Route');
+  res.send('Bad Route');
 });
 
 var server = app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
+
+// // var http = require('http'); //add the http module
+// // var myServer = http.createServer(function(request, response) {
+// //   response.writeHead(200, {"Content-Type" : "text/html"});
+// //   response.write("<b>Hello</b> World 2");
+// //   response.end();
+// // }); //create a server
+
+
+
+// // myServer.listen(3000);
+
+// // console.log("Go to http://localhost:3000 on your browser");
+
+
+// // var express = require('express');
+// // var app = express();
+
+// // app.get('/', function(req, res) {
+// //   res.send('<H1>Hello</H1> Express');
+// // });
+
+// // var server = app.listen(3000, function() {
+// //   console.log('Listening on port 3000');
+// // });
+
+
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function(req, res) {
+//   res.send('<H1>Hello</H1> Express');
+// });
+
+// app.get('/me', function(req, res) {
+//   res.send('@planetoftheweb');
+// });
+
+// app.get('/who/:name?', function(req, res) {
+//   var name = req.params.name;
+//   res.send(name + ' was here');
+// });
+
+// app.get('/who/:name?/:title?', function(req, res) {
+//   var name = req.params.name;
+//   var title = req.params.title;
+//   res.send('<p>name: ' + name + '<br>title: ' + title + '</p>');
+// });
+
+
+// app.get(['/redirect','/godaddy'], function(req, res) {
+//   res.redirect('http://www.godaddy.com');
+// });
+
+// app.get('*', function(req, res) {
+//   res.send(404,'Bad Route');
+// });
+
+// var server = app.listen(3000, function() {
+//   console.log('Listening on port 3000');
+// });
