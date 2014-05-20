924f77fd5afd7d3207ef351686895e46b43d2a1c
Dominic Tuso <dtuso@godaddy.com>
2014-05-20 11:20:41 -0700

    Update app.js to use the new ExpressJs module

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-var http = require('http'); //add the http module
-var myServer = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type" : "text/html"});
-  response.write("<b>Hello</b> World 2");
-  response.end();
-}); //create a server
+// var http = require('http'); //add the http module
+// var myServer = http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type" : "text/html"});
+//   response.write("<b>Hello</b> World 2");
+//   response.end();
+// }); //create a server
 
 
 
-myServer.listen(3000);
+// myServer.listen(3000);
 
-console.log("Go to http://localhost:3000 on your browser");
+// console.log("Go to http://localhost:3000 on your browser");
+
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('<H1>Hello</H1> Express');
+});
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
